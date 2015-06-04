9d6040fac734a23ea6b45c9f641a2fcdae5ef654
Matthew Reuther <matthew.reuther@gmail.com>
2015-06-04 00:06:19 -0500

    finally figured out stickyjs and have navbar working the way I want it to, styled about page and now need to work on my skills page

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,7 @@ $(function() {
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      target = target.length ? target : $('[name=' + this.hash.slice(4) +']');
       if (target.length) {
         $('html,body').animate({
           scrollTop: target.offset().top
@@ -12,3 +12,25 @@ $(function() {
     }
   });
 });
+
+$(document).ready(function() {
+  var navOffset = $(".nav").offset().top;
+  // alert(navOffset);
+
+  $(".nav").wrap('<div class="nav-placeholder"></div>');
+  $(".nav-placeholder").height($(".nav").outerHeight());
+
+  $(".nav").wrapInner('<div class="nav-inner"></div>');
+
+  $(window).scroll(function() {
+    var scrollPosition = $(window).scrollTop();
+      // $(".status").html(scrollPosition);
+    if (scrollPosition >= navOffset) {
+      $(".nav").addClass("fixed");
+    }
+    else {
+      $(".nav").removeClass("fixed");
+    }
+  });
+});
+
